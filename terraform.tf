53f4cd2a92c1facf60d1873914d69bae7a55777f
MorelSami <mksami237@gmail.com>
2024-05-06 11:00:46 -0400

    terraform update

--- terraform.tf
+++ terraform.tf
@@ -1,35 +1,44 @@
 # Define the provider for AWS
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
 }
 
 # Create an ECS cluster
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "nodejs-ecs-cluster"  
+  name = "nodejs-ecs-cluster"
 }
 
 # Create a task definition
 resource "aws_ecs_task_definition" "ecs_task_definition" {
-  family                = "nodejs-family-test"
+  family                   = "nodejs-family-test"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 1024
   memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions = <<EOF
-[
-  {
-    "name": "nodejs-app-container",
-    "image": "smorel237/nodejs-webapp:v1.0.0",  
-    "portMappings": [
+  container_definitions = jsonencode(
+    [
       {
-        "containerPort": 3000,
-        "hostPort": 3000
+        name  = "nodejs-app-container"
+        image = "smorel237/nodejs-webapp:v1.0.0"
+        portMappings = [
+          {
+            containerPort = 3000
+            hostPort      = 3000
+            protocol      = "tcp"
+          }
+        ]
       }
     ]
-  }
-]
-EOF
+  )
+}
+
+#create alb target group for task definition service
+resource "aws_lb_target_group" "ecs_target_group" {
+  name     = "nodejs-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-091393213ec10604f"
 }
 
 # Create a service to run the task on the cluster
@@ -39,16 +48,23 @@ resource "aws_ecs_service" "my_service" {
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   desired_count   = 2
   launch_type     = "FARGATE"
+
   network_configuration {
-    subnets          = ["subnet-08c54a97347812875"]  
-    security_groups  = ["sg-0b702e58ad3103075"]      
+    subnets          = ["subnet-08c54a97347812875"]
+    security_groups  = ["sg-0b702e58ad3103075"]
     assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+    container_name   = "nodejs-app-container"
+    container_port   = 3000
   }
 }
 
 # Create an ecs task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role-nodejs"
+  name               = "ecs_task_execution_role-nodejs"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
